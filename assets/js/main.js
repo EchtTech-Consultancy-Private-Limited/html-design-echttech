fac44d95eeefdd21a3f28d583ba1463b4150964e
harsh-echttech <harsh.shukla@echttech.com>
2025-02-03 18:26:20 +0530

    changes in ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,18 @@ $(document).ready(function () {
     gallery: { enabled: true }
   });
 });
+
+
+// font size changes
+let defaultSize = 16// Default font size
+
+function changeSize(change) {
+  let elements = document.documentElement;
+  let currentSize = parseInt(window.getComputedStyle(elements).fontSize);
+  elements.style.fontSize = (currentSize + change) + "px";
+}
+
+function resetSize() {
+  let elements = document.documentElement;
+  elements.style.fontSize = defaultSize + "px";
+};
